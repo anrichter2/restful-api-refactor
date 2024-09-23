c188445426c80a23f8944da98c5c0c5edade6edc
anrichter <anrichter2@gmail.com>
2024-09-22 21:27:06 -0500

    Filled out resolvers.js file in schemas folder of server. Also changed auth middleware function of server utils to work with GraphQL.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleWare } = require('@apollo/server/express');
-const { authMiddleWare, authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 const routes = require('./routes');
